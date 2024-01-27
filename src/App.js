45d809b33388fe4e10068a7995f810ffec7ca3db
Kirill <manfeed3@gmail.com>
2024-01-27 12:32:02 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Aside from "./comp/Aside.jsx"
+import Header from "./comp/Header.jsx"
+import Profile from "./comp/Profile.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <Header />
+      <Aside />
+      <Profile />
     </div>
   );
 }
---
+++ src/comp/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Header = () => {
+    return (<header className="header">
+        <img src="./images/ow/logo.png" />
+        <h1>OtherBook</h1>
+    </header>)
+}
+
+export default Header;
